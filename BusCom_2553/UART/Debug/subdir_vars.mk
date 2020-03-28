2327ba192caaf5674c2912f8c265552bb57ee3db
Alex Gonnet <alex.gonnet@groupe-esigelec.org>
2020-03-28 13:24:11 +0100

    add 2231 directory

---
+++ BusCom_2553/UART/Debug/subdir_vars.mk
@@ -0,0 +1,41 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430g2553.cmd 
+
+C_SRCS += \
+../ADC.c \
+../Afficheur.c \
+../main.c 
+
+C_DEPS += \
+./ADC.d \
+./Afficheur.d \
+./main.d 
+
+OBJS += \
+./ADC.obj \
+./Afficheur.obj \
+./main.obj 
+
+OBJS__QUOTED += \
+"ADC.obj" \
+"Afficheur.obj" \
+"main.obj" 
+
+C_DEPS__QUOTED += \
+"ADC.d" \
+"Afficheur.d" \
+"main.d" 
+
+C_SRCS__QUOTED += \
+"../ADC.c" \
+"../Afficheur.c" \
+"../main.c" 
+
+
