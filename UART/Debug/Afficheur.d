6565ff7f1957fb2683a233f1192e09f6a2841922
Alex Gonnet <alex.gonnet@groupe-esigelec.org>
2020-03-28 12:58:52 +0100

    interface uart debut

---
+++ UART/Debug/Afficheur.d
@@ -0,0 +1,17 @@
+# FIXED
+
+Afficheur.obj: ../Afficheur.c
+Afficheur.obj: C:/ti/ccsv7/ccs_base/msp430/include/msp430.h
+Afficheur.obj: C:/ti/ccsv7/ccs_base/msp430/include/msp430g2553.h
+Afficheur.obj: C:/ti/ccsv7/ccs_base/msp430/include/in430.h
+Afficheur.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/intrinsics.h
+Afficheur.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/intrinsics_legacy_undefs.h
+Afficheur.obj: ../Afficheur.h
+
+../Afficheur.c: 
+C:/ti/ccsv7/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccsv7/ccs_base/msp430/include/msp430g2553.h: 
+C:/ti/ccsv7/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/intrinsics.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/intrinsics_legacy_undefs.h: 
+../Afficheur.h: 
