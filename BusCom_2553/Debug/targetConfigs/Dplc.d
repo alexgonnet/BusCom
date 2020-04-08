6f5d6dd015d669af4d5c5fa01ed1e5ff25bda9f7
Alex Gonnet <alex.gonnet@groupe-esigelec.org>
2020-04-08 11:51:42 +0200

    organise 2553

---
+++ BusCom_2553/Debug/targetConfigs/Dplc.d
@@ -0,0 +1,15 @@
+# FIXED
+
+targetConfigs/Dplc.obj: ../targetConfigs/Dplc.c
+targetConfigs/Dplc.obj: C:/ti/ccsv7/ccs_base/msp430/include/msp430.h
+targetConfigs/Dplc.obj: C:/ti/ccsv7/ccs_base/msp430/include/msp430g2553.h
+targetConfigs/Dplc.obj: C:/ti/ccsv7/ccs_base/msp430/include/in430.h
+targetConfigs/Dplc.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/intrinsics.h
+targetConfigs/Dplc.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/intrinsics_legacy_undefs.h
+
+../targetConfigs/Dplc.c: 
+C:/ti/ccsv7/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccsv7/ccs_base/msp430/include/msp430g2553.h: 
+C:/ti/ccsv7/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/intrinsics.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/intrinsics_legacy_undefs.h: 
---
+++ BusCom_2553/Debug/targetConfigs/subdir_vars.mk
@@ -0,0 +1,26 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../targetConfigs/Dplc.c 
+
+C_DEPS += \
+./targetConfigs/Dplc.d 
+
+OBJS += \
+./targetConfigs/Dplc.obj 
+
+OBJS__QUOTED += \
+"targetConfigs\Dplc.obj" 
+
+C_DEPS__QUOTED += \
+"targetConfigs\Dplc.d" 
+
+C_SRCS__QUOTED += \
+"../targetConfigs/Dplc.c" 
+
+
