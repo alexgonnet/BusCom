e8f634d981174e6f48250a1e39aaf4a4eb08dada
codeben20 <benjamin.bourg@groupe-esigelec.org>
2020-04-08 12:06:25 +0200

    Comments IR

--- Complements/capteur_ir/main.c
+++ Complements/capteur_ir/main.c
@@ -1,17 +1,14 @@
 #include <msp430g2231.h>
 #include "ADC.h"
 
-int capt= 0;
-int  infra_centre= 0;
 
-
-
+/* Timer initialization */
 void init_Timer( void )
 {
-TACTL &= ~MC_0; // arret du timer
+TACTL &= ~MC_0; /* Stop the timer */
 TACCR0 = 62500;
-TACTL = (0 | TASSEL_2 | MC_3 | ID_3 | TACLR); // select TimerA source SMCLK, set mode to up-counting
-TACCTL0 = 0 | OUTMOD_7 | CCIE; // select timer compare mode
+TACTL = (0 | TASSEL_2 | MC_3 | ID_3 | TACLR); /* select TimerA source SMCLK, set mode to up-counting*/
+TACCTL0 = 0 | OUTMOD_7 | CCIE; /* select timer compare mode*/
 }
 
 
@@ -21,33 +18,19 @@ TACCTL0 = 0 | OUTMOD_7 | CCIE; // select timer compare mode
  */
 int main(void)
 {
-    WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+    WDTCTL = WDTPW | WDTHOLD;	/* stop watchdog timer*/
     ADC_init();
 
     init_Timer();
 
-    P1SEL &= ~(BIT0 | BIT6); // Port 1, ligne 0 et 6 en fonction primaire
-    P1DIR |= (BIT0 | BIT6 ); // P1.0 et P1.6 en sortie
-    P1OUT &= ~(BIT0 | BIT6); // P1.0 et P1.6 à 0
+    P1SEL &= ~(BIT0 | BIT6); /* Port 1, line 0 and 6 in primary function*/
+    P1DIR |= (BIT0 | BIT6 ); /* P1.0 and P1.6 as output*/
+    P1OUT &= ~(BIT0 | BIT6); /* P1.0 and P1.6 to 0*/
 
-    P1DIR &= ~BIT3;             //P1.3 en entrée pour l'interrupteur
-    P1SEL &= ~BIT3;             // en mode O/I
+    P1DIR &= ~BIT3;             /*P1.3 input for interruption*/
+    P1SEL &= ~BIT3;             /* O/I mode */
 
-/*
-    //timer pour asservissement
-   // TACTL &= ~MC_0; // arret du timer
-
-    TACTL=0|(TASSEL_2|ID_3);//sourceSMCLK,pasdepredivisionID_0
-    TACTL|=MC_3;//comptageenmodeupdown
-
-    //TACTL |= TACLR;
-
-    TACTL|=TAIE;//autorisationinterruptionTAIE
-    TACCR1=62500;//periode timer pour 1s
-
-    TACCTL1 = 0 | OUTMOD_7 | CCIE; // select timer compare mode
-*/
-    __enable_interrupt();//Activation des interruptions
+    __enable_interrupt();/*Enable interruptions*/
 
 
 
@@ -61,28 +44,23 @@ int main(void)
 
 
 
-
-#pragma vector=TIMERA0_VECTOR //timer utilisé pour lire les données des capteurs
+/* timer used to read the sensor value */
+#pragma vector=TIMERA0_VECTOR
 __interrupt void detect(void)
 {
-    //Equilibre();
-    ADC_Demarrer_conversion(0x03); // on lit l'infrarouge du centre
-    if(ADC_Lire_resultat() > 500) // si la valeur est dessus du seuil, on a un obstacle
+    ADC_Demarrer_conversion(0x03); /* Read the sensor value */
+    if(ADC_Lire_resultat() > 500) /* If the value is above the limit value */
     {
-        P1OUT &=~BIT6; // Eteindre la Led du port 1 ligne 6
-        if(infra_centre < 51) // on vérifie plusieurs fois la valeur pour éviter les erreurs
-            infra_centre++;
-        P1OUT ^= BIT0; // Faire clignoter la Led a chaque itération
+        P1OUT &=~BIT6; /*Switch off the TED P1.6*/
+        P1OUT ^= BIT0; /* Flash the LED on each iteration*/
 
     }
     else
     {
-        P1OUT &=~BIT0; // Eteindre la Led du port 1 ligne 6
-        if(infra_centre > -51)
-            infra_centre--;
-        P1OUT ^= BIT6; // Faire clignoter la Led a chaque itération
+        P1OUT &=~BIT0; /* Switch off the TED P1.0*/
+        P1OUT ^= BIT6; /* Flash the LED on each iteration*/
     }
 
 
-    TACTL &= ~TAIFG; //RAZ TAIFG
+    TACTL &= ~TAIFG; /*RAZ TAIFG*/
 }
