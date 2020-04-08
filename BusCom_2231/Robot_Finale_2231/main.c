b5794f9947a563bc3c25db9c1caf4a28b36ef0c3
codeben20 <benjamin.bourg@groupe-esigelec.org>
2020-04-08 09:34:18 +0200

    Add commentaires

--- BusCom_2231/Robot_Finale_2231/main.c
+++ BusCom_2231/Robot_Finale_2231/main.c
@@ -1,21 +1,14 @@
 /*
- * -----------------------------------------------------
- *  P R O G R A M M A T I O N   S P I   E S I G E L E C
  *
- * Lauchpad v1.5 support
+ * M S P 4 3 0 G 2 2 3 1   -   SPI 3 Wires
  *
- * M S P 4 3 0 G 2 2 3 1   -   SPI/SLAVE 3 Wires
- *
- * (c)-Yann DUCHEMIN / ESIGELEC - r.III162018 for CCS
+ * BOURG Benjamin
+ * GONNET Alex
  * --------------------------------------------------------------
  * La carte Launchpad est raccordée en SPI via l'USI B0
  *      SCLK : P1.5 / SCLK
  *      SIMO : P1.7 / SDI
  *      MOSI : P1.6 / SDO
- *
- * A la reception du caractère 1 la LED Rouge P1.0 est allumée
- *
- * A la reception du caractère 0 la LED Rouge P1.0 est eteinte
  *
  */
 #include <msp430.h>
@@ -24,16 +17,17 @@
 #include "ADC.h"
 #include "send.h"
 
-volatile unsigned char RXDta;
-unsigned int capt = 0;
-unsigned int state = 0;
+volatile unsigned char RXDta; /* Store the receive char */
+unsigned int capt = 0; /* Store the sensor value */
+unsigned int state = 0; /* State of the IR sensor : Active or Inactive */
 
+/* Init the timer */
 void init_Timer( void )
 {
-    TACTL &= ~MC_0; // arret du timer
+    TACTL &= ~MC_0; /* arret du timer*/
     TACCR0 = 62500;
-    TACTL = (0 | TASSEL_2 | MC_3 | ID_3 | TACLR); // select TimerA source SMCLK, set mode to up-counting
-    TACCTL0 = 0 | OUTMOD_7 | CCIE; // select timer compare mode
+    TACTL = (0 | TASSEL_2 | MC_3 | ID_3 | TACLR); /* select TimerA source SMCLK, set mode to up-counting*/
+    TACCTL0 = 0 | OUTMOD_7 | CCIE; /* select timer compare mode*/
 }
 
 
@@ -43,7 +37,7 @@ void init_Timer( void )
  */
 void main( void )
 {
-    // Stop watchdog timer to prevent time out reset
+    /* Stop watchdog timer to prevent time out reset*/
     WDTCTL = WDTPW | WDTHOLD;
 
     if(CALBC1_1MHZ==0xFF || CALDCO_1MHZ==0xFF)
@@ -52,32 +46,33 @@ void main( void )
     }
     else
     {
-        // Factory Set.
+        /* Factory Set. */
         DCOCTL = 0;
         BCSCTL1 = CALBC1_1MHZ;
         DCOCTL = (0 | CALDCO_1MHZ);
     }
 
 
-
+    /* Init SPI, ADC and timer */
     init_send();
     ADC_init();
     init_Timer();
-    // Wait for the SPI clock to be idle (low).
+    /* Wait for the SPI clock to be idle (low).*/
     while ((P1IN & BIT5)) ;
 
     USICTL0 &= ~USISWRST;
 
-    //__bis_SR_register(LPM4_bits | GIE); // general interrupts enable & Low Power Mode
     __enable_interrupt();
     while(1)
     {
+        /* If sensor active */
         if(state == 1)
         {
+            /* If there is a danger */
             if(capt > 500){
                 capt = 0;
                 TACCR0 = 0;
-                Send_char_SPI('x');//Valeur d'arret
+                Send_char_SPI('x');/*Stop value*/
                 TACCR0 = 62500;
             }
         }
@@ -92,39 +87,36 @@ void main( void )
 #pragma vector=USI_VECTOR
 __interrupt void universal_serial_interface(void)
 {
-    while( !(USICTL1 & USIIFG) );   // waiting char by USI counter flag
+    while( !(USICTL1 & USIIFG) );   /* waiting char by USI counter flag */
     RXDta = USISRL;
 
-    if (RXDta == 0x01) //if the input buffer is 0x31 (mainly to read the buffer)
+    if (RXDta == 0x01) /*if the input buffer is 0x01 = Active the sensor */
     {
         state = 1;
-        P1OUT |= BIT0; //turn on LED
-        //USISRL = 0x00;
+        P1OUT |= BIT0; /*turn on LED*/
     }
-    else if (RXDta == 0x00)
+    else if (RXDta == 0x00) /* Turn off the sensor */
     {
         state = 0;
-        P1OUT &= ~BIT0; //turn off LED
-        //USISRL = 0x00;
+        P1OUT &= ~BIT0; /*turn off LED*/
     }
 
     USISRL = '\0';
-    USICNT &= ~USI16B;  // re-load counter & ignore USISRH
-    USICNT = 0x08;      // 8 bits count, that re-enable USI for next transfert
+    USICNT &= ~USI16B;  /* re-load counter & ignore USISRH*/
+    USICNT = 0x08;      /* 8 bits count, that re-enable USI for next transfert*/
 }
-//------------------------------------------------------------------ End ISR
 
 
 
-
-#pragma vector=TIMERA0_VECTOR //timer utilisé pour lire les données des capteurs
+/* Use to read IR sensor values */
+#pragma vector=TIMERA0_VECTOR
 __interrupt void detect(void)
 {
-    ADC_Demarrer_conversion(0x03); // on lit l'infrarouge du centre
+    ADC_Demarrer_conversion(0x03); /* Read the sensor return value on BIT3*/
     capt = ADC_Lire_resultat();
 
 
-    TACTL &= ~TAIFG; //RAZ TAIFG
+    TACTL &= ~TAIFG; /*RAZ TAIFG*/
 }
 
 
--- BusCom_2231/Robot_Finale_2231/send.c
+++ BusCom_2231/Robot_Finale_2231/send.c
@@ -1,79 +1,56 @@
+/*
+ * BOURG Benjamin
+ * GONNET Alex
+ *
+ * Send throw SPI value
+ */
+
 #include <msp430g2231.h>
 
-
+/* Function to init the send and receive char from/to SPI */
 void init_send(){
-   /* P1OUT |= 0x06;
-    P1DIR |= 0x06;
+    /*--------------- Secure mode*/
+        P1SEL = 0x00;        /* GPIO */
+        P1DIR = 0x00;         /* IN */
 
-
-    // USI Config. for SPI 3 wires Slave Op.
-    // P1SEL Ref. p41,42 SLAS694J used by USIPEx
-    USICTL0 |= USISWRST;
-    USICTL1 = 0;
-
-    // 3 wire, mode Clk&Ph / 14.2.3 p400
-    // SDI-SDO-SCLK - LSB First - Output Enable - Transp. Latch
-   // USICTL0 |= (USIPE7 | USIPE6 | USIPE5 | USIMST | USIOE);
-
-    USICTL1 |= USICKPH + USIIE;               // Counter interrupt, flag remains set
-    USICKCTL = USIDIV_1 + USISSEL_2;          // /2 SMCLK
-*/
- /*   USICNT = 0;
-    USICNT &= ~(USI16B | USIIFGCC ); // Only lower 8 bits used 14.2.3.3 p 401 slau144j
-    USISRL = 0x23;  // hash, just mean ready; USISRL Vs USIR by ~USI16B set to 0
-    USICNT = 0x08;
-    */
-  /*  USICNT &= ~(USI16B | USIIFGCC );
-
-    USISRL = 0x23;  // hash, just mean ready; USISRL Vs USIR by ~USI16B set to 0
-
-   USICTL0 |= USIPE7 + USIPE6 + USIPE5 + USIMST + USIOE; // Port, SPI Master
-    USICTL0 &= ~USISWRST;                     // USI released for operation
-    USICNT = 8;
-*/
-
-    //--------------- Secure mode
-        P1SEL = 0x00;        // GPIO
-        P1DIR = 0x00;         // IN
-
-        // led
+        /* led */
         P1DIR |=  BIT0;
         P1OUT &= ~BIT0;
 
-        // USI Config. for SPI 3 wires Slave Op.
-        // P1SEL Ref. p41,42 SLAS694J used by USIPEx
+        /* USI Config. for SPI 3 wires Slave Op.*/
+        /* P1SEL Ref. p41,42 SLAS694J used by USIPEx*/
         USICTL0 |= USISWRST;
         USICTL1 = 0;
 
-        // 3 wire, mode Clk&Ph / 14.2.3 p400
-        // SDI-SDO-SCLK - LSB First - Output Enable - Transp. Latch
+        /* 3 wire, mode Clk&Ph / 14.2.3 p400 */
+        /* SDI-SDO-SCLK - LSB First - Output Enable - Transp. Latch */
         USICTL0 |= (USIPE7 | USIPE6 | USIPE5 | USILSB | USIOE | USIGE );
-        // Slave Mode SLAU144J 14.2.3.2 p400
+        /* Slave Mode SLAU144J 14.2.3.2 p400 */
         USICTL0 &= ~(USIMST);
         USICTL1 |= USIIE;
         USICTL1 &= ~(USICKPH | USII2C);
 
-        USICKCTL = 0;           // No Clk Src in slave mode
-        USICKCTL &= ~(USICKPL | USISWCLK);  // Polarity - Input ClkLow
+        USICKCTL = 0;           /* No Clk Src in slave mode */
+        USICKCTL &= ~(USICKPL | USISWCLK);  /* Polarity - Input ClkLow */
 
         USICNT = 0;
-        USICNT &= ~(USI16B | USIIFGCC ); // Only lower 8 bits used 14.2.3.3 p 401 slau144j
-        USISRL = 0x23;  // hash, just mean ready; USISRL Vs USIR by ~USI16B set to 0
+        USICNT &= ~(USI16B | USIIFGCC ); /* Only lower 8 bits used 14.2.3.3 p 401 slau144j */
+        USISRL = 0x23;  /* hash, just mean ready; USISRL Vs USIR by ~USI16B set to 0 */
         USICNT = 0x08;
 }
 
 
 
-
+/* Function to send char throw SPI*/
 void Send_char_SPI(unsigned char carac)
 {
-    while (!(USIIFG & USICTL1));   // attend que USCI_SPI soit dispo.
+    while (!(USIIFG & USICTL1));   /* attend que USCI_SPI soit dispo. */
     USISRL = carac;
     USICNT &= ~USI16B;
     USICNT = 8;
 }
 
-
+/* Function to send a string of char */
 void Send_chaine_SPI(unsigned char *msg)
 {
     unsigned int i = 0;
@@ -81,5 +58,5 @@ void Send_chaine_SPI(unsigned char *msg)
     {
         Send_char_SPI(msg[i]);
     }
-    Send_char_SPI('z');//Caractère de fin de message
+    Send_char_SPI('z');/* Caractère de fin de message */
 }
